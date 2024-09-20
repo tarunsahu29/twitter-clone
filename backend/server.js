74a0d2caeeac8a2973610f65afc47e46c8cabe98
Tarun Sahu <tarun929s@Taruns-Air.bbrouter>
2024-09-20 22:32:15 +0530

    signup functionality and jwt authentication

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,9 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json()) // to parse req.body
+app.use(express.urlencoded({ extended: true })) //to parse form data(urlencoded)
+
 app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {

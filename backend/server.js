eb4a418b05ace54fbb3d19c5ff91efcc9eccbd1e
Tarun Sahu <tarun929s@Taruns-Air.bbrouter>
2024-09-20 18:29:49 +0530

    Initial configuration and Database connection completed

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+import authRoutes from './routes/auth.routes.js'
+import connectMongoDB from './db/connectMongoDB.js'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`)
+  connectMongoDB()
+})

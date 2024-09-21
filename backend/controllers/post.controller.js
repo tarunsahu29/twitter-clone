52c65f96f56484df7efd19bb61eb365debf7baec
Tarun Sahu <tarun929s@Taruns-Air.bbrouter>
2024-09-22 01:51:18 +0530

    getLikedPosts functionality completed

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -126,11 +126,13 @@ export const likeUnlikePost = async (req, res) => {
 
     if (userLikedPost) {
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } })
+      await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } })
       res.status(200).json({
         message: 'Post unliked successfully',
       })
     } else {
       post.likes.push(userId)
+      await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } })
       await post.save()
 
       const notification = new Notification({
@@ -174,6 +176,38 @@ export const getAllPosts = async (req, res) => {
     res.status(200).json(posts)
   } catch (error) {
     console.log('Error in getAllPosts controller : ', error)
+    res.status(400).json({
+      error: 'Internal Server Error',
+    })
+  }
+}
+
+export const getLikedPosts = async (req, res) => {
+  const userId = req.params.id
+
+  try {
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(404).json({
+        error: 'User not found',
+      })
+    }
+
+    const likedPosts = await Post.find({
+      _id: { $in: user.likedPosts },
+    })
+      .populate({
+        path: 'user',
+        select: '-password',
+      })
+      .populate({
+        path: 'comments.user',
+        select: '-password',
+      })
+
+    res.status(200).json(likedPosts)
+  } catch (error) {
+    console.log('Error in getLikedposts controller : ', error)
     res.status(400).json({
       error: 'Internal Server Error',
     })

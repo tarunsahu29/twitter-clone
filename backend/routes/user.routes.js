76c2e90564c5326f40d484d8ac21ef83ab6f2ea4
Tarun Sahu <tarun929s@Taruns-Air.bbrouter>
2024-09-21 17:53:59 +0530

    configured cloudinary and completed update user functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,7 @@ import {
   followUnfollowUser,
   getSuggestedUsers,
   getUserProfile,
+  updateUser,
 } from '../controllers/user.controller.js'
 
 const router = express.Router()
@@ -11,6 +12,6 @@ const router = express.Router()
 router.get('/profile/:username', protectRoute, getUserProfile)
 router.get('/suggested', protectRoute, getSuggestedUsers)
 router.post('/follow/:id', protectRoute, followUnfollowUser)
-router.post('/update', protectRoute)
+router.post('/update', protectRoute, updateUser)
 
 export default router

666867dcf37a3026bd1e51fc040065feae67d7f2
Tarun Sahu <tarun929s@Taruns-Air.bbrouter>
2024-09-24 17:21:32 +0530

    login and signup frontend connected to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import ProfilePage from './pages/profile/ProfilePage'
 
 import Sidebar from './components/common/Sidebar'
 import RightPanel from './components/common/RightPanel'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/profile/:username" element={<ProfilePage />} />
       </Routes>
       <RightPanel />
+      <Toaster />
     </div>
   )
 }

16b8dd1842dd843b6f6781de4277fa57323b8a23
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-09-27 00:56:13 +0530

    follow/unfollow functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -47,8 +47,6 @@ export const followUnfollowUser = async (req, res) => {
       // Unfollow the user
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } })
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } })
-
-      // TODO: return the id of the user as a response
       res.status(200).json({
         message: 'User unfollowed successfully',
       })
@@ -64,7 +62,6 @@ export const followUnfollowUser = async (req, res) => {
       })
 
       await newNotification.save()
-      // TODO: return the id of the user as a response
       res.status(200).json({
         message: 'User followed successfully',
       })

6e4a5b11efc9d93984399d6bc874ca96dfe35b21
Tarun Sahu <tarun929s@Taruns-MacBook-Air.local>
2024-09-27 06:42:42 +0530

    deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { v2 as cloudinary } from 'cloudinary'
@@ -20,6 +21,7 @@ cloudinary.config({
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: '5mb' })) // to parse req.body
 app.use(express.urlencoded({ extended: true })) //to parse form data(urlencoded)
@@ -30,6 +32,14 @@ app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
 app.use('/api/notifications', notificationRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`)
